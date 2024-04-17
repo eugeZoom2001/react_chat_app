14407c2427be243f33318fe321d99fa73ba66ff9
eugeZoom2001 <72229718+eugeZoom2001@users.noreply.github.com>
2024-04-17 12:41:07 -0300

    Create users.js

---
+++ server/test/users.js
@@ -0,0 +1,9 @@
+const users_admin = [
+    { user: "euge", email: "eugenionoche2@gmail.com" },
+    { user: "axel", email: "axel@gmail.com" },
+    { user: "pedro", email: "pedro@gmail.com" },
+    { user: "lucia", email: "lucia@gmail.com" },
+    { user: "maria", email: "maria@gmail.com" },
+];
+
+module.exports = { users_admin };
